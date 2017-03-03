8963b0b768f9c042f783f39ac94c7d237ecf7d32
PineChang <1034638186@qq.com>
2017-03-03 10:01:05 +0800

    完成流式标签布局

--- FlowTags/app/src/main/java/com/infosky/flowtags/MainActivity.java
+++ FlowTags/app/src/main/java/com/infosky/flowtags/MainActivity.java
@@ -2,12 +2,37 @@ package com.infosky.flowtags;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.infosky.flowtags.view.FlowLayout;
 
 public class MainActivity extends AppCompatActivity {
+    FlowLayout mFlowlayout;
 
+    String[] tagArray = {"sldkflklk","sjlkdljfklj;","jdlkjlfjljkldjfkldjflk","sjlkdljfklj;","sjlkdljfklj;","sjlkdljfklj;"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mFlowlayout = (FlowLayout)findViewById(R.id.fl_layout);
+        Button btn;
+        for (int i = 0; i < tagArray.length; i++) {
+            btn = new Button(this);
+            ViewGroup.MarginLayoutParams lp = new ViewGroup.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT,
+                    ViewGroup.MarginLayoutParams.WRAP_CONTENT);
+            //让这些添加进去的Button都是WrapContent,那么在测量阶段他的最大宽度也就是父容器的宽度
+            lp.topMargin = 15;
+            lp.leftMargin = 15;
+            btn.setBackground(getResources().getDrawable(R.drawable.tv_bg));
+            btn.setLayoutParams(lp);
+            btn.setText(tagArray[i]);
+            //每次添加子view的时候都会重新进行测量,布局,绘制;
+            mFlowlayout.addView(btn);
+
+
+            
+        }
     }
 }
--- FlowTags/app/src/main/java/com/infosky/flowtags/view/FlowLayout.java
+++ FlowTags/app/src/main/java/com/infosky/flowtags/view/FlowLayout.java
@@ -1,15 +1,21 @@
 package com.infosky.flowtags.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by PineChang on 2017/3/2.
+ * 实现思路是先把每一行的View存起来
+ * 再把每一行的行高存起来
+ * 然后就能算到每一行中某个元素确定位置了,
  */
 
 public class FlowLayout extends ViewGroup {
@@ -60,7 +66,7 @@ public class FlowLayout extends ViewGroup {
             //如果当前已经用到的累加的行宽和要遍历的到的子view宽度之和大于总宽度减去父view左右内边距,
             //那么就换新行
             if(childWidth+lineWidth>sizeWidth-getPaddingLeft()-getPaddingRight()){
-                width = Math.max(width,lineWidth)//比较n-1上次的lineWidth和n-2-0从中的最大值中取得最大值
+                width = Math.max(width,lineWidth);//比较n-1上次的lineWidth和n-2-0从中的最大值中取得最大值
                 lineWidth = childWidth;//此时是新行的的lineWidth
                 height +=lineHeight;//加上当前行高
                 lineHeight = childHeight;
@@ -107,14 +113,79 @@ public class FlowLayout extends ViewGroup {
                     //把上一行的的行高加进去,以及把上一行的所有views加进去
                     mLineHeight.add(lineHeight);
                     mAllViews.add(lineViews);
+                    //换行后本行的行宽
+                    lineWidth = 0;//清零行高,也就是在0的基础上增加行高
+                    //换行后本行没有任何元素
+                    lineViews = new ArrayList<>();
+                    //换行后,当前行的行高为测量后的行高
+                    lineHeight = childHeight;//变为下一行的上一行行高;
+                }
+                //本行要加的行宽,换行的情况下,行宽在0的基础上添加,不换行的直接添加
+                lineWidth += childWidth+lp.leftMargin+lp.rightMargin;
+                lineViews.add(child);
 
-                    lineWidth = 0;
-                    lineHeight = childHeight
+                lineHeight = Math.max(lineHeight,childHeight+lp.topMargin+lp.bottomMargin);
+
+                if (child instanceof TextView) {
+                    ((TextView)child).setTextColor(getColor());
+
                 }
 
+            }
+            //遍历完成后,最后一行的行高和和行元素还没有添加进去,所以添加进去
+            mLineHeight.add(lineHeight);
+            mAllViews.add(lineViews);
+            //--------------------------------------------------------------
+            //在计算完了每一行的行高,以及这一行对应的元素后,开始布局
+            int left = getPaddingLeft();//父view的left内边距
+            int top = getPaddingTop();//父view的顶！顶！d=====(￣▽￣*)b边距
+            int lineCount  = mAllViews.size();//总共有多上行
+            for (int i = 0; i < lineCount; i++) {
+                //以每一行为单位开始布局
+                lineViews  = mAllViews.get(i);//拿到第i行的元素
+                lineHeight = mLineHeight.get(i);//拿到第i行的行高
+                for (int j = 0; j < lineViews.size(); j++) {
+                    //拿到第i行第j个元素
+                    child = lineViews.get(j);
+                    if (child.getVisibility() == View.GONE) {
+                        //如果这个button的可见性为不可见,那么直接略过
+                        //因为在测量的阶段这个child就没有布局参数
+                        continue;
+                    }
+                    //因为child中getMeasuredWidth或者Height不包括margin,所以就要获取
+                    //因为在android中,所有的box为borderbox
+                    MarginLayoutParams lp = (MarginLayoutParams)child.getLayoutParams();
+                    int lc = left + lp.leftMargin;
+                    int tc  = top +lp.topMargin;
+                    int rc = lc+ child.getMeasuredWidth();
+                    int bc = tc + child.getMeasuredHeight();
+                    child.layout(lc,tc,rc,bc);
+                    left +=child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;
+                }
+                //当一行遍历完成后,那么就将左坐标变为父容器的pading值,
+                //上坐标变为父容器上padding加行高
+                left = getPaddingLeft();
+                top +=lineHeight;
 
             }
 
+
         }
+    }
+    private int getColor(){
+        //
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        String temp;
+        for (int i = 0; i < 3; i++) {
+            //将一个整型变为一个十六进制的字符串
+            temp = Integer.toHexString(random.nextInt(0xff));
+            if(temp.length()==1){
+                //16进制可能是1位的,那么就将其在前面加0
+                temp = "0"+temp;
+            }
+            sb.append(temp);
+        }
+        return Color.parseColor("#"+sb.toString());
     }
 }
